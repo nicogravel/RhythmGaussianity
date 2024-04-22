a3e5f83eb1fe0ebc42971605429d41c5b478f35a
nicogravel <nicolas.gravel@gmail.com>
2024-04-22 15:17:16 +0200

    added results summary

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Rhythm Gaussianity
 
-Revealing Rhythm Gaussianity by using the Coefficient of Variation of the Envelope (CVE).
+Here a minimal working example for revealing Rhythm Gaussianity by relating amplitude modulations to signal energy.
 
 * CVE = 0.523 reflects Gaussian noise
 * CVE < 0.523 reflects rhythmic fluctuations (e.g. Kuramoto oscillations)
@@ -14,26 +14,39 @@ Revealing Rhythm Gaussianity by using the Coefficient of Variation of the Envelo
 
 2. Hidalgo VM, Letelier JC, Diaz J. **The amplitude modulation pattern of Gaussian noise is a fingerprint of Gaussianity**. https://doi.org/10.48550/arXiv.2203.16253
 
-Here we provide two minimal working examples. A *toolbox free* example using Matlab for illustrative purpose (mwe folder), and a working example using Matlab and the toolbox Fieldtrip (mwe_fieldtrip folder).
 
-## **Example results**
 
-The following plots were obtained from EEG data for a single subject (sampling rate: 1000Hz). Electrode signals from five occipital and parietal electrodes were re-referenced to the global average and band-pass filtered between 8-13 Hz using a zero-shift Butterworth filter of order 4. The complex analytical signal was computed using the Hilbert transform applied to the whole time series length. The CVE, RMS envelope and Kuramoto order parameters were computed for sliding windows of 24 seconds length and 90% overlap (this must change to 50% when scaling up the analysis). No artifacts rejection was applied. In the following plots, the capital letters A and B indicate arbitrary conditions. In parenthesis, the number of sliding windows for each condition (#).
+## **Example analysis**
+
+## *Preprocessing of EEG data*
+Electrode signals from five occipital and parietal electrodes in 20 subjects in B and A conditions were re-referenced to the global average, normalized within channels, and band-pass filtered between 8-14 Hz using a zero-shift Butter-worth filter of order 4. The complex analytical signal was computed using the Hilbert transform applied to the whole time series length. The coefficient of variation and the root median square (CVE and RMS) of the envelope were computed for sliding windows of 4 seconds length and 25% overlap. No artifacts rejection was applied. The coefficient of variation of the envelope (CVE) was computed as the standard deviation divided by the mean of the envelope within each contiguous window. 
+
+### *RMS envelope as a function of CVE*  
+For each condition, the RMS envelope and the CVE  values for each contiguous window were grouped across subjects, binned (N=100) and plotted as histograms (Figure 1). To compare the RMS envelope and CVE distributions between conditions, we used two-sampled t-test. Subsequently, to visualize the relationship between RMS envelope and the CVE in each condition, bi-variate histograms were obtained as follows. First, the RMS and CVE values for each window were pooled across subjects and the two conditions to obtain global minimum and the maximum values for each estimate. Second, for each condition, the RMS envelope and CVE from each window were grouped into the cells of a two-dimensional grid defined in CVE bins of 0.005 between 0 and 1 and RMS envelope bins of 0.04 times the global maximum CVE between 0 and the global maximum value of the CVE (obtained from both conditions as explained previously). This step provides with a common histogram grid for both condition. Third, to compare the resulting bi-variate distributions (the resulting bin counts in each each condition), we used contour plots and normalized them using the maximum bin count (Figure 2). 
+
+<img src="https://github.com/nicogravel/RhythmGaussianity/blob/main/figures/Fig1.png" width=100%>
+
+**Figure 1.** (A) Distribution of power in the alpha band (RMS envelope). The EEG signal for  was  (B) Distribution of the coefficient of variation of the envelope. Alpha power significantly decreased in the A, compared to that of the B condition, whereas the CVE distribution were not significantly different. Light red corresponds to the A condition. 
+
+### *Shifts in RMS and CVE*  
+To asses the tendency in each condition to decrease or increase power (RMS) and CVE throughout the recordings, we quantified sequences of shifts in the RMS sign. First, we defined a minimum sequence length (3 consecutive windows, equivalent to 9 seconds) and identified the start of each sequence. Second, we summed the number of sequences with positive or negative shifts. Third, we computed the percentage of signed sequence shifts above the minimum sequence length.  
  
-### **RMS envelope as a function of CVE and the Kuramoto order parameter for phase relations**  
-  
-For data normalized *across* electrodes (preserves the topographical magnitude scaling, misses amplitude-dependent patterns):     
-<img src="https://github.com/nicogravel/RhythmGaussianity/blob/main/mwe_fieldtrip/rmsenv-cve_alpha_across.png" width=50%><img src="https://github.com/nicogravel/RhythmGaussianity/blob/main/mwe_fieldtrip/kurvar-rms_alpha_across.png" width=50%>
+### *State occupancy* 
+To quantify the range of dynamic activity in each conditions captured by the CVE, we proceeded as follows. First, we computed the global CVE shifts (all window shifts pooled for each condition). This gave us a symmetric distribution centered at zero.  Second, we obtained the 5% and 95% percentile values of this distribution and used them to define low and high CVE thresholds by subtracting or adding them (respectively) to the CVE of Gaussian noise (√(4 − π)/π ≈ 0.523). This allowed us to compute, for each condition, the number of temporal windows occupying the lower or higher ends of the global CVE space. Values below 0.523 in CVE indicate rhythmic activity whereas higher values indicate phasic activity.
 
-For data normalized *within* electrodes (works as magnitude correction, makes amplitude-dependent patterns comparable):    
-<img src="https://github.com/nicogravel/RhythmGaussianity/blob/main/mwe_fieldtrip/rmsenv-cve_alpha_within.png" width=50%><img src="https://github.com/nicogravel/RhythmGaussianity/blob/main/mwe_fieldtrip/kurvar-rms_alpha_within.png" width=50%>
+<img src="https://github.com/nicogravel/RhythmGaussianity/blob/main/figures/Fig2.png" width=75%>
 
-### **Distributions for CVE and the standard deviation of the Kuramoto order parameter for phase relations**  
-  
-Light-red and grey histograms corresponds to condition A and B, respectively:  
-<img src="https://github.com/nicogravel/RhythmGaussianity/blob/main/mwe_fieldtrip/hist_cve_alpha.png" width=50%><img src="https://github.com/nicogravel/RhythmGaussianity/blob/main/mwe_fieldtrip/kurvar_alpha.png" width=50%>
+**Figure 2.** Contour plots reflecting the relationship between EEG amplitude and CVE.  Distribution of power in the alpha band (RMS envelope in the y-axis) as a function of the coefficient of variation of the envelope (CVE in the x-axis) for temporal windows pooled across subjects. Histogram bin counts were made comparable between the A (A) and B condition (B) by normalizing the bi-variate grid and setting the colormap to the maximum bin count. Contours were obtained by linear interpolation of the bi-variate grid. Colorbar indicate bin count for each contour.
+	
+|   |   **A**| **B**  | 
+|---|---|---|
+|   RMS increases|    39.6825 %|    61.2613 %|  
+|   RMS decreases|    60.3175 %|     38.7387 %|   
+|   CVE increases|   52.1739 %|   48.3871 %|  
+|   CVE decreases|    47.8261 %|   51.6129 %|
+**Table 1.** Changes in RMS envelope and CVE in point to departures from rhythm Gaussianity. To quantify the percentage of increases in RMS envelope and CVE departures n
 
-### **The Kuramoto order parameter for phase relations overlaid onto an histogram of phase differences**  
-  
-Example for one condition: 
-<img src="https://github.com/nicogravel/RhythmGaussianity/blob/main/mwe_fieldtrip/kurHist_alpha_condB.png" width=100%>
+
+### *Results and concluding remarks*
+The mean of the RMS envelope distributions differed between the B and the A condition (two-sampled t-test, p-val <0.0001). On the other hand, the mean of the CVE distributions did not (two-sampled t-test, p-val = 0.0011). For sequences of contiguous windows above 9 seconds, positive RMS shifts were increased in the B condition. Events longer than 9 seconds in which alpha power decreased were 60.3175% for the A condition and 38.7387% for the B condition. In the other hand, alpha power increasing events were 39.6825% for the A condition and 61.2613% for the B condition. These results indicate that alpha power increases was somewhat suppressed for A.  In the case of CVE, results told a different story. For sequences of contiguous windows above 9 seconds, positive CVE shifts were increased in the A condition. Events longer than 9 seconds in which CVE decreased were 47.8261% for the A condition and 51.6129% for the B condition. In the other hand, CVE increasing events were 52.1739% for the A condition and 48.3871% for the B condition. This indicates that departures from the alpha rhythm Gaussianity in the direction of phasic activity were higher in the A condition, possibly pointing to increased neuronal avalanches. On the other hand, departures towards oscillatory behavior were increased in the B conditions. Finally, state occupancy revealed a reduced repertoire for A, with occupancy values of 1.1788 % and 2.3772% of the total window count for rhythmic and phasic activity, respectively, compared to 2.1998% and 3.593% in the B condition. Regardless of the reduced dynamic range in RMS and CVE (revealed by our state occupancy analysis), our analysis of RMS sequence shifts confirmed decreases in alpha power for the A condition. Moreover, in the A condition, CVE sequence shifts revealed increased tendency of event departing from Gaussianity towards phasic activity. This last result is of fundamental methodological importance, as it reveals, in a data driven manner, departures in the dynamic repertoire of rhythmic itineraries, likely pointing to mechanisms such as neuronal avalanches, hypothesized to occur more frequently in the psychedelic state. 
+
---
+++ Summarize_Results.m
@@ -0,0 +1,340 @@
+%% Using the Coefficient of Variation (CVE) and RMS envelope to assess Rhythm Gaussianity
+% CVE = 0.523 reflects Gaussian noise
+% CVE < 0.523 reflects rhythmic fluctuations (e.g. Kuramoto oscillations)
+% CVE > 0.523 reflects phasic activity (e.g. avalanches)
+%
+% Authors:
+% Nicolas Gravel <nicolas.gravel@gmail.com>
+%
+% References:
+%
+% Hidalgo VM, Diaz J, Mpodozis J, Letelier JC. Envelope Analysis of
+% the Human Alpha Rhythm Reveals EEG Gaussianity. IEEE Trans Biomed Eng.
+% 2023 Apr;70(4):1242-1251.
+% doi: 10.1109/TBME.2022.3213840. Epub 2023 Mar 21. PMID: 36223351.
+%
+% Hidalgo VM, Letelier JC, Diaz J. The amplitude modulation pattern of
+% Gaussian noise is a fingerprint of Gaussianity.
+% https://doi.org/10.48550/arXiv.2203.16253
+
+
+clear; close; clc; %close all
+
+%load('RESULT.mat')
+
+
+
+%% Load results
+for condition = 1 : length(list.conditions)
+    fprintf('\n Printing %s:\n', list.conditions{condition})
+    data = eval(['results.' list.conditions{condition}]);
+    for file = 1: length(data)
+        CVE{condition,file}  =  data{file}.cve(:); % CVE
+        RMS{condition,file}  =  data{file}.pow(:); % RMS envelope
+        COH{condition,file} =  data{file}.kurVar_ch(:); % RMS envelope\
+        KUR{condition,file}  =  data{file}.kur_all(:); % RMS envelope
+    end
+end
+
+%% Compute differences in RMS and CVE
+% RMS
+Y_a =  vertcat(RMS{1,:}); Y_b =  vertcat(RMS{2,:});
+[H0, pval] = ttest2(Y_a,Y_b,'tail','left','alpha',0.001)  % RMS differences are significative
+% CVE
+X_a =  vertcat(CVE{1,:}); X_b =  vertcat(CVE{2,:});    
+[H0,pval] = ttest2(X_a,X_b,'tail','left','alpha',0.001)  % CVE  differences are not significative
+
+%% Compute shifts in RMS and CVE for succesive temporal windows
+for i = 1:length(Y_a)-1; dY_a(i) = Y_a(i) - Y_a(i+1); end
+for i = 1:length(Y_b)-1; dY_b(i) = Y_b(i) -Y_b(i+1); end
+mean(dY_a)
+mean(dY_b)
+for i = 1:length(X_a)-1; dX_a(i) = X_a(i) - X_a(i+1); end
+for i = 1:length(X_b)-1; dX_b(i) = X_b(i) -X_b(i+1); end
+mean(dX_a)
+mean(dX_b)
+
+%% Compute sequences of RMS shift sign: decreases or increases in power
+excursion_length = 3 % sequence length as number of succesive windows, here 12 seconds
+%% Go ahead...
+% Conditions A
+Ex = dY_a < 0;
+max_excursionLength = max(diff(find([1,diff(Ex),1]))); % empirical 
+runs = diff(find([1,diff(Ex),1])); starts = find([1,diff(Ex)]);
+%Excursions = Ex(starts(find(runs == excursion_length)));
+Excursions = Ex(starts(runs > excursion_length));
+decreases_A   = length(nonzeros(Excursions));
+increases_A   = length(nonzeros(~Excursions));
+% Percentage of events across all windows with length >= excursion_length
+power_dyn_A(1,:) = (100/length(Excursions))*decreases_A; % = 44.9541% of alpha power decreasing events longer than excursion_length
+power_dyn_A(2,:) = (100/length(Excursions))*increases_A;  % = 55.0459% of alpha power increasing events longer than excursion_length
+%(100/length(Excursions))*decreases_A  + (100/length(Excursions))*increases_A % Sanity check 
+power_dyn_A
+
+% Conditions B
+Ex = dY_b < 0;
+max_excursionLength = max(diff(find([1,diff(Ex),1]))); % empirical 
+runs = diff(find([1,diff(Ex),1])); starts = find([1,diff(Ex)]);
+%Excursions = Ex(starts(find(runs == excursion_length)));
+Excursions = Ex(starts(runs > excursion_length));
+decreases_B   = length(nonzeros(Excursions));
+increases_B   = length(nonzeros(~Excursions));
+% Percentage of events across all windows with length >= excursion_length
+power_dyn_B(1,:) = (100/length(Excursions))*decreases_B; % = 40.5172% of alpha power decreasing events longer than excursion_length
+power_dyn_B(2,:) = (100/length(Excursions))*increases_B;  % = 59.4829% of alpha power increasing events longer than excursion_length
+%(100/length(Excursions))*decreases_B  + (100/length(Excursions))*increases_B % Sanity check 
+power_dyn_B
+
+%% Compute sequences of CVE sign shift: departures from rhythm Gaussianity
+%% Go ahead...
+% Conditions A
+Ex = dX_a > 0;
+max_excursionLength = max(diff(find([1,diff(Ex),1])));
+runs = diff(find([1,diff(Ex),1])); starts = find([1,diff(Ex)]);
+%Excursions = Ex(starts(find(runs == excursion_length)));
+Excursions = Ex(starts(runs > excursion_length));
+towards_rhythm_A  = length(nonzeros(Excursions));
+towards_pulses_A   = length(nonzeros(~Excursions));
+% Percentage of events across all windows with length > excursion_length
+cve_dyn_A(1,:) =  (100/length(Excursions))*towards_rhythm_A; % = 52.1739% of "towards rhythm events" longer than excursion_length
+cve_dyn_A(2,:) =  (100/length(Excursions))*towards_pulses_A; % = 47.8261% of "towards pulses" events longer than excursion_length
+%(100/length(Excursions))*towards_rhythm_A  + (100/length(Excursions))*towards_pulses_A % Sanity check 
+cve_dyn_A
+
+% Conditions B
+Ex = dX_b > 0;
+max_excursionLength = max(diff(find([1,diff(Ex),1])));
+runs = diff(find([1,diff(Ex),1])); starts = find([1,diff(Ex)]);
+%Excursions = Ex(starts(find(runs == excursion_length)));
+Excursions = Ex(starts(runs > excursion_length));
+towards_rhythm_B  = length(nonzeros(Excursions));
+towards_pulses_B   = length(nonzeros(~Excursions));
+% Percentage of events across all windows with length > excursion_length
+cve_dyn_B(1,:) =  (100/length(Excursions))*towards_rhythm_B; % = 48.3871% of "towards rhythm events" longer than excursion_length
+cve_dyn_B(2,:) =  (100/length(Excursions))*towards_pulses_B; % = 51.6129% of "towards pulses" events longer than excursion_length
+%(100/length(Excursions))*towards_rhythm_B  + (100/length(Excursions))*towards_pulses_B % Sanity check 
+cve_dyn_B
+
+%% Occupancy: windows below or above 5% and 95% percentiles in CVE for each conditions
+Gaussian_CVE = sqrt((4-pi)/pi) % Gaussian CVE
+cut_left   = prctile([dX_a,dX_b],5); cut_right = prctile([dX_a,dX_b],95); % percentile cut-offs
+thr_left   = Gaussian_CVE + cut_left; thr_right = Gaussian_CVE + cut_right; % thresholds
+rhythmicity_a = X_a(X_a <= thr_left); rhythmicity_b = X_b(X_b <= thr_left); % rhythmic windows
+phasic_a = X_a(X_a >= thr_right); phasic_b = X_b(X_b >= thr_right); % phasic windows
+rhythmic_windows(1,:)= length(rhythmicity_a); rhythmic_windows(2,:)= length(rhythmicity_b);
+rhythmic_occupancy(1,:) = (100/length(X_a))*rhythmic_windows(1,:);
+rhythmic_occupancy(2,:) = (100/length(X_b))*rhythmic_windows(2,:);
+rhythmic_occupancy % percentage of rhytmic windows for each condition
+phasic_windows(1,:) = length(phasic_a); phasic_windows(2,:) = length(phasic_b);
+phasic_occupancy(1,:) = (100/length(X_a))*phasic_windows(1,:);
+phasic_occupancy(2,:) = (100/length(X_b))*phasic_windows(2,:);
+phasic_occupancy % percentage of phasic windows for each condition
+
+
+%% Metastability
+kuramoto = KUR(1,:);
+metastability(1,:) = std(reshape(cell2mat(vertcat(kuramoto{:})).',1,[]));
+kuramoto = KUR(2,:);
+metastability(2,:) = std(reshape(cell2mat(vertcat(kuramoto{:})).',1,[]));
+metastability
+
+
+%% CVE histograms
+figure,
+pos = get(gcf, 'Position');
+set(gcf, 'Position', [pos(1)-600 pos(2) 600, 250]); % Set plot size
+set(gcf, 'color', 'w'); % Set figure background
+Xa =  Y_a; %results{1}.cve(:);
+Xb =  Y_b; %results{2}.cve(:);
+C=min([Xa;Xb]):(max([Xa;Xb])/100):max([Xa;Xb]);
+[Ns]=histc(Xa,C);
+relativefreqNs = Ns ./ sum(Ns);
+b(1) = bar(C,relativefreqNs,'histc');
+hold on
+[Ns]=histc(Xb,C);
+relativefreqNs = Ns ./ sum(Ns);
+b(2) = bar(C,relativefreqNs,'histc');
+xlabel('RMS','FontSize', 14);
+ylabel('Relative frequency','FontSize', 14);
+%xlim([0.5 1]);
+b = findobj(gca,'Type','patch');
+set(b(1),'FaceColor', 'k','EdgeColor', 'k','facealpha',0.5,'edgealpha',0);
+set(b(2),'FaceColor', 'r','EdgeColor', 'r','facealpha',0.5,'edgealpha',0);
+set(gca, 'FontSize', 14);
+set(gca,'LineWidth',1)
+folder = pwd;
+fname = [folder '/hist_rms_alpha_all.png'];
+print(gcf, fname, '-dpng', '-r150', '-painters')
+
+figure,
+pos = get(gcf, 'Position');
+set(gcf, 'Position', [pos(1) pos(2) 600, 250]); % Set plot size
+set(gcf, 'color', 'w'); % Set figure background
+Xa =  X_a; %results{1}.cve(:);
+Xb =  X_b; %results{2}.cve(:);
+C=min([Xa;Xb]):(max([Xa;Xb])/100):max([Xa;Xb]);
+[Ns]=histc(Xa,C);
+relativefreqNs = Ns ./ sum(Ns);
+b(1) = bar(C,relativefreqNs,'histc');
+hold on
+[Ns]=histc(Xb,C);
+relativefreqNs = Ns ./ sum(Ns);
+b(2) = bar(C,relativefreqNs,'histc');
+xlabel('CVE','FontSize', 14);
+ylabel('Relative frequency','FontSize', 14);
+xlim([0 1]);
+b = findobj(gca,'Type','patch');
+set(b(1),'FaceColor', 'k','EdgeColor', 'k','facealpha',0.5,'edgealpha',0);
+set(b(2),'FaceColor', 'r','EdgeColor', 'r','facealpha',0.5,'edgealpha',0);
+set(gca, 'FontSize', 14);
+set(gca,'LineWidth',1)
+folder = pwd;
+fname = [folder '/hist_cve_alpha_all.png'];
+print(gcf, fname, '-dpng', '-r150', '-painters')
+
+% CVE shifts
+figure,
+pos = get(gcf, 'Position');
+set(gcf, 'Position', [pos(1)+600 pos(2) 600, 250]); % Set plot size
+set(gcf, 'color', 'w'); % Set figure background
+Xa =  dX_a';
+Xb =  dX_b';
+C=min([Xa;Xb]):(max([Xa;Xb])/100):max([Xa;Xb]);
+[Ns]=histc(Xa,C);
+relativefreqNs = Ns ./ sum(Ns);
+b(1) = bar(C,relativefreqNs,'histc');
+hold on
+[Ns]=histc(Xb,C);
+relativefreqNs = Ns ./ sum(Ns);
+b(2) = bar(C,relativefreqNs,'histc');
+xlabel('CVE shifts','FontSize', 14);
+ylabel('Relative frequency','FontSize', 14);
+%xlim([0.5 1]);
+b = findobj(gca,'Type','patch');
+set(b(1),'FaceColor', 'k','EdgeColor', 'k','facealpha',0.5,'edgealpha',0);
+set(b(2),'FaceColor', 'r','EdgeColor', 'r','facealpha',0.5,'edgealpha',0);
+set(gca, 'FontSize', 14);
+set(gca,'LineWidth',1)
+folder = pwd;
+fname = [folder '/hist_cve_shifts_all.png'];
+print(gcf, fname, '-dpng', '-r150', '-painters')
+
+% Kuramoto variance histograms
+figure,
+pos = get(gcf, 'Position');
+set(gcf, 'Position', [pos(1)-400 pos(2)-100 600, 250]); % Set plot size
+set(gcf, 'color', 'w'); % Set figure background
+Xa =  vertcat(COH{1,:}); % Kuramoto
+Xb =  vertcat(COH{2,:}); % Kuramoto
+C=0:(max([Xa;Xb])/50):max([Xa;Xb]);
+[Ns]=histc(Xa,C);
+relativefreqNs = Ns ./ sum(Ns);
+b(1) = bar(C,relativefreqNs,'histc');
+hold on
+[Ns]=histc(Xb,C);
+relativefreqNs = Ns ./ sum(Ns);
+b(2) = bar(C,relativefreqNs,'histc');
+xlabel(['std({\Delta}{\phi} coherence (R) )'],'FontSize', 14);
+ylabel('Relative frequency','FontSize', 14);
+%xlim([0.5 1]);
+b = findobj(gca,'Type','patch');
+set(b(1),'FaceColor', 'k','EdgeColor', 'k','facealpha',0.5,'edgealpha',0);
+set(b(2),'FaceColor', 'r','EdgeColor', 'r','facealpha',0.5,'edgealpha',0);
+set(gca, 'FontSize', 14);
+set(gca,'LineWidth',1)
+folder = pwd
+fname = [folder '/kurvar_alpha_all.png'];
+print(gcf, fname, '-dpng', '-r150', '-painters')
+
+
+%% Plot RMS envelope as function of CVE
+figure,
+pos = get(gcf, 'Position');
+set(gcf, 'Position', [pos(1)-400 pos(2)-100 800, 400]); % Set plot size
+set(gcf, 'color', 'w'); % Set figure background
+% The set both axis (conditions) with the same range
+X_1 =  vertcat(CVE{1,:}); % CVE
+Y_1 =  vertcat(RMS{1,:}); % RMS envelope
+X_2 =  vertcat(CVE{2,:}); % CVE
+Y_2 =  vertcat(RMS{2,:});  % RMS envelope
+minX  = min([X_1(:); X_2(:)]); maxX  = max([X_1(:); X_2(:)]);
+minY  = min([Y_1(:); Y_2(:)]); maxY  = max([Y_1(:); Y_2(:)]);
+binfactor = 25;
+maxCount = 250;
+for cond_i = 1:2
+    subplot(1,2,cond_i);
+    X =  vertcat(CVE{cond_i,:}); % CVE
+    Y =  vertcat(RMS{cond_i,:}); % RMS envelope
+    minP  = minY; %min(Y(:));
+    maxP  = maxY; %max(Y(:));
+    bin = length(Y)/binfactor;
+    [counts, bin_centers] = hist3([X,Y],'Ctrs',{0:0.05:1 0:maxP/binfactor:maxP});
+    [~,c] = contourf(bin_centers{1},bin_centers{2}, counts');
+    c.LineWidth = 0.0001;
+    %pcolor(bin_centers{1},bin_centers{2}, counts');
+    line([0.523 0.523],[minY maxY],'Color',[1 0 0])
+    hold on
+    xlim([0 1]);
+    %ylim([minY maxY]);
+    ylim([minY 2]);
+    axis square;
+    colorbar;
+    caxis([0 maxCount]);
+    %title([list.conditions(cond_i) ' ( #' num2str(length(results{cond_i}.time)) ')'],'FontSize', 14);
+    xlabel('CVE','FontSize', 14);
+    ylabel('RMS envelope','FontSize', 14);
+    set(gca,'FontSize', 14);
+    set(gca,'LineWidth',1)
+    clear X Y
+end
+colormap(flipud(bone))
+folder = pwd;
+fname = [folder '/rmsenv-cve_alpha_all.png'];
+print(gcf, fname, '-dpng', '-r150', '-painters')
+
+
+%% Plot phase coherence as function of RMS envelope
+figure,
+pos = get(gcf, 'Position');
+set(gcf, 'Position', [pos(1)+400 pos(2)+200 800, 400]); % Set plot size
+set(gcf, 'color', 'w'); % Set figure background
+X_1 =  vertcat(COH{1,:}); % Phase coherence
+Y_1 =  vertcat(RMS{1,:}); % RMS envelope
+X_2 =  vertcat(COH{2,:}); % Phase coherence
+Y_2 =  vertcat(RMS{2,:});  % RMS envelope
+minX  = min([X_1(:); X_2(:)]); maxX  = max([X_1(:); X_2(:)]);
+minY  = min([Y_1(:); Y_2(:)]); maxY  = max([Y_1(:); Y_2(:)]);
+binfactor = 25;
+for cond_i = 1:2
+    subplot(1,2,cond_i);
+    %X =  results{cond_i}.kurVar_ch(:);  % Phase coherence
+    %Y =  results{cond_i}.pow(:);        % RMS envelope
+    X =  vertcat(COH{cond_i,:}); % Phase coherence
+    Y =  vertcat(RMS{cond_i,:});  % RMS envelope
+    minP  = min(Y(:));
+    maxP  = max(Y(:));
+    bin = length(Y)/binfactor;
+    [counts, bin_centers] = hist3([X,Y],'Ctrs',{0:0.05:1 0:maxP/binfactor:maxP});
+    [~,c] = contourf(bin_centers{1},bin_centers{2}, counts');
+    c.LineWidth = 0.0001;
+    %pcolor(bin_centers{1},bin_centers{2}, counts');
+    hold on
+    line([0 maxX],[0 maxY],'Color',[1 0 0])
+    axis square;
+    colorbar;
+    %title([conditions(cond_i) ' ( #' num2str(length(results{cond_i}.time)) ')'],'FontSize', 14);
+    ylabel('RMS envelope','FontSize', 14);
+    xlabel(['std({\Delta}{\phi} coherence (R) )'],'FontSize', 14);
+    set(gca, 'FontSize', 14);
+    set(gca,'LineWidth',1)
+    xlim([0 maxX]);
+    ylim([0 maxY]);
+    clear X Y
+end
+colormap(flipud(bone))
+folder = pwd
+fname = [folder '/kurvar-rms_alpha_all.png'];
+print(gcf, fname, '-dpng', '-r150', '-painters')
+
+
+
